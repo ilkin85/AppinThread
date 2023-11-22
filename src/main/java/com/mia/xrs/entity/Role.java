89c8dd89fc43e794563f415a526555b226902acf
Zamir Seferov <zamirseferov@gmail.com>
2023-11-22 11:10:30 +0400

    Entity

---
+++ src/main/java/com/mia/xrs/entity/Role.java
@@ -0,0 +1,27 @@
+package com.mia.xrs.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String authority;
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
---
+++ src/main/java/com/mia/xrs/entity/Zone.java
@@ -0,0 +1,17 @@
+package com.mia.xrs.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Zone {
+}
