bb1beb3423de2b17ed511d40349a2a73020f4199
https://github.com/Resid-Huseynov/demo.git <residhuseynov763@gmail.com>
2023-11-22 09:40:47 +0400

    Initial commit

---
+++ src/main/java/com/mia/xrs/XrsApplication.java
@@ -0,0 +1,13 @@
+package com.mia.xrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XrsApplication.class, args);
+	}
+
+}
