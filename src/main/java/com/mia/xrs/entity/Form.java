98f031a91e75b334c5ed9c9cde2927d60074596a
ilkin <ilkinelesgerli85@gmail.com>
2023-12-05 10:57:56 +0400

    ilkin

---
+++ src/main/java/com/mia/xrs/entity/Form.java
@@ -0,0 +1,22 @@
+package com.mia.xrs.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Form {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String formNumber;
+
+}
