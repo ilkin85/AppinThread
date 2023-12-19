edca2c84fd865a08c62f216a994a948a899a0f26
https://github.com/Resid-Huseynov/demo.git <residhuseynov763@gmail.com>
2023-12-19 14:02:40 +0400

    rjctcntrl

--- src/main/java/com/mia/xrs/repository/RejectRepository.java
+++ src/main/java/com/mia/xrs/repository/RejectRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 public interface RejectRepository extends JpaRepository<Reject,Integer> {
